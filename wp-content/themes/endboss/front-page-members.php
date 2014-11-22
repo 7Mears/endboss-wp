c02dff918f6ba1054a2d47eaca9595f92c888a91
dansee <astandardcandle@gmail.com>
2014-11-22 12:53:34 -0600

    Name spaced member custom post type

--- wp-content/themes/endboss/front-page-members.php
+++ wp-content/themes/endboss/front-page-members.php
@@ -1,5 +1,5 @@
     <?php
-        $args = array( 'post_type' => 'member', 'posts_per_page' => 10 );
+        $args = array( 'post_type' => 'endboss_member', 'posts_per_page' => 10 );
         $loop = new WP_Query( $args );
         while ( $loop->have_posts() ) : $loop->the_post();?>
 
